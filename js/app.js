2a851dbe5a254e53891a5f59e78234cd3bd1c76a
Daniel Huie <huied93@uw.edu>
2014-11-07 00:09:24 -0800

    final updates

--- js/app.js
+++ js/app.js
@@ -1,30 +1,3 @@
-//logic ideas
-//
-//have a button that when clicked, game is completely reset
-//
-//if one tile is already flipped over, you must flip over another one
-//once two tiles have been flipped, simultaneously flip them back over to their default state
-//
-//keep track of tile name (its the number) and set a variable inside the loop to check if two tiles match each other, if they match, increment the respective counters
-//
-//keep track of whether or not tile can be flipped over. Two states: initial move when attempting to find a pair of tiles OR when two tiles have already been matched
-//
-//keep track of a counter that tallies the number of tiles matched. start with 8 (8 pairs) and decrement by 1 whenever 2 images are matched. if the counter == 0, stop the time and show the congratulations message
-//
-//misc. variables
-//matches made
-//matches missed
-//matches remaining
-//
-//a turn is when two DIFFERENT tiles are clicked on
-//
-//if two tiles are a mismatch, flip them back over
-//
-//must use window.setTimeout() somewhere
-
-
-// if you want to change hasFoundMatch in two tiles, you must reference the tiles array and change it directly there
-
 $(document).ready(function() {
     var matchesMade = 0;
     var matchesMissed = 0;
@@ -34,70 +7,114 @@ $(document).ready(function() {
     var numClicks = 0;
     var turnBackOver = false;
     var prevImg;
+    var gameBoard = $('#game-board');
 
-    $('#elapsed-seconds').text("Seconds Elapsed: 0");
-    $('#matches-made').text("Matches Made: 0");
-    $('#matches-remaining').text("Matches Remaining: 8");
-    $('#matches-attempted').text("Matches Attempted: 0");
+    // timer to show elapsed time on page
+    var startTime = _.now();
+    var timer = window.setInterval(function() {
+        var elapsedSeconds = 0 + Math.floor((_.now() - startTime) / 1000);
+        $('#elapsed-seconds').text("Seconds Elapsed: " + elapsedSeconds);
+        if (matchesLeft == 0) {
+            window.clearInterval(timer);
+            $("#congrats").css("display", "block");
+        }
+    }, 1000);
 
-    var tiles = [];
-    var i;
-    for (i = 1; i <= 32; i++) {
-        tiles.push({
-            tileNum: i,
-            src: 'img/tile' + i + '.jpg',
-            hasFoundMatch: false
+    displayStats();
+    setUp();
+
+    function setUp() {
+        var tiles = [];
+        var i;
+        for (i = 1; i <= 32; i++) {
+            tiles.push({
+                tileNum: i,
+                src: 'img/tile' + i + '.jpg',
+                hasFoundMatch: false
+            });
+        }
+
+        var shuffledTiles = _.shuffle(tiles);
+        var selectedTiles = shuffledTiles.slice(0, 8);
+        var tilePairs = [];
+        _.forEach(selectedTiles, function(tile) {
+            tilePairs.push(_.clone(tile));
+            tilePairs.push(_.clone(tile));
         });
+
+        tilePairs = _.shuffle(tilePairs);
+        var row = $(document.createElement('div'));
+        var img;
+
+        // sets up the game board
+        _.forEach(tilePairs, function(tile, elemIndex) {
+            // for double equal expressions, place the static value before the test
+            // triple equals does type comparison (strings, objects, etc)
+            if (elemIndex > 0 && 0 == elemIndex % 4) {
+                gameBoard.append(row);
+                row = $(document.createElement('div'));
+            }
+            img = $(document.createElement('img'));
+            img.attr({
+                src: 'img/tile-back.png',
+                alt: 'image of tile ' + tile.tileNum
+            });
+            img.data('tile', tile);
+            row.append(img);
+        });
+        gameBoard.append(row);
     }
 
-    var shuffledTiles = _.shuffle(tiles);
+    function click() {
+        // registers the click function upon clicking a tile
+        $('#game-board img').click(function() {
+            // 'this' refers to the single HTML DOM element that just got clicked on
+            var img = $(this);
+            var tile = img.data('tile');
 
-    var selectedTiles = shuffledTiles.slice(0, 8);
-
-    var tilePairs = [];
-    _.forEach(selectedTiles, function(tile) {
-        tilePairs.push(_.clone(tile));
-        tilePairs.push(_.clone(tile));
-    });
-
-    //tilePairs = _.shuffle(tilePairs);
-
-    //  selects element by id
-    var gameBoard = $('#game-board');
-    var row = $(document.createElement('div'));
-    var img;
-
-    // sets up the game board
-    _.forEach(tilePairs, function(tile, elemIndex) {
-        // for double equal expressions, place the static value before the test
-        // triple equals does type comparison (strings, objects, etc)
-        if (elemIndex > 0 && 0 == elemIndex % 4) {
-            gameBoard.append(row);
-            row = $(document.createElement('div'));
-        }
-        img = $(document.createElement('img'));
-        img.attr({
-            src: 'img/tile-back.png',
-            alt: 'image of tile ' + tile.tileNum
-        });
-        img.data('tile', tile);
-        row.append(img);
-    });
-    gameBoard.append(row);
-
-    $('#btn2').click(function() {
-
-    });
-
+            clickAction(img, tile);
+            displayUpdatedStats();
+        }); //on click of gameboard images
+    }
 
     // registers the click function upon clicking a tile
     $('#game-board img').click(function() {
         // 'this' refers to the single HTML DOM element that just got clicked on
         var img = $(this);
         var tile = img.data('tile');
+        clickAction(img, tile);
+        displayUpdatedStats();
+    }); //on click of gameboard images
 
-        console.log("Can turn back over? " +turnBackOver);
+    // resets game
+    $('#new-game').click(function() {
+        matchesMade = 0;
+        matchesMissed = 0;
+        matchesLeft = 8;
+        prev = null;
+        curr = null;
+        numClicks = 0;
+        turnBackOver = false;
+        prevImg = null;
 
+        $("#congrats").css("display", "none");
+        displayStats();
+
+        startTime = _.now();
+        timer = window.setInterval(function() {
+            elapsedSeconds = 0 + Math.floor((_.now() - startTime) / 1000);
+            $('#elapsed-seconds').text("Seconds Elapsed: " + elapsedSeconds);
+            if (matchesLeft == 0) {
+                window.clearInterval(timer);
+            }
+        }, 1000);
+
+        gameBoard.empty();
+        setUp();
+        click();
+    });
+
+    function clickAction(img, tile) {
         if (numClicks == 2) {
             numClicks = 0;
             turnBackOver = false;
@@ -114,7 +131,6 @@ $(document).ready(function() {
             numClicks++;
             curr = tile;
             img.attr('src', tile.src);
-
             if (prev.tileNum === curr.tileNum) {
                 prev.hasFoundMatch = true;
                 tile.hasFoundMatch = true;
@@ -128,29 +144,23 @@ $(document).ready(function() {
                 setTimeout(function(){
                     img.attr('src', 'img/tile-back.png');
                     prevImg.attr('src', 'img/tile-back.png');
-                    console.log("can i finally flip the two tiles back over? " + turnBackOver);
                     prev = null;
                     curr = null;
                 }, 1000);
             }
         }
+    }
 
+    function displayStats() {
+        $('#elapsed-seconds').text("Seconds Elapsed: 0");
+        $('#matches-made').text("Matches Made: 0");
+        $('#matches-remaining').text("Matches Remaining: 8");
+        $('#matches-attempted').text("Matches Missed: 0");
+    }
+
+    function displayUpdatedStats() {
         $('#matches-made').text("Matches Made: " + matchesMade);
         $('#matches-remaining').text("Matches Remaining: " + matchesLeft);
-        $('#matches-attempted').text("Matches Attempted: " + matchesMissed);
-    }); //on click of gameboard images
-
-    // timer to show elapsed time on page
-    var startTime = _.now();
-    var timer = window.setInterval(function() {
-        var elapsedSeconds = 0 + Math.floor((_.now() - startTime) / 1000);
-        $('#elapsed-seconds').text("Seconds Elapsed: " + elapsedSeconds);
-
-        if (matchesLeft == 0) {
-            window.clearInterval(timer);
-            console.log("Congratulations!");
-        }
-    }, 1000);
-
-
+        $('#matches-attempted').text("Matches Missed: " + matchesMissed);
+    }
 });
